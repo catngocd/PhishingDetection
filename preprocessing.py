b657c43eb144c897d364708eea5b6984d52c1515
catngocd <catngocnd@gmail.com>
2019-12-01 20:14:43 -0500

    added shuffling to preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -3,8 +3,10 @@ import numpy as np
 
 def preprocess_all(csv_files, is_phishing):
     '''
-    csv_file: list of strings that is the csv_file name
-    is_phishing: boolean array
+    Inputs:
+        csv_file: a list of strings where each string is the name of a csv file 
+        is_phishing: a Boolean array where each boolean represents whether the file contains phishing link
+    Output: (shuffled features from all csv files, shuffled labels of all csv files)
     '''
     all_features = []
     all_labels = []
@@ -22,7 +24,25 @@ def preprocess_all(csv_files, is_phishing):
     all_features = np.array(all_features)
     all_labels = np.array(all_labels)
 
+    all_features = shuffle_all(all_features)
+    all_labels = shuffle_all(all_labels)
+
     return all_features, all_labels
+
+def shuffle_all(features, labels):
+    '''
+    Inputs:
+        features: a list of list of feautres
+        labels: a list of all the labels
+    Output: (shuffled features, shuffled labels)
+    '''
+    indices = np.array(range(len(labels)))
+    np.random.shuffle(indices)
+
+    features = features[indices]
+    labels = labels[indices]
+
+    return features, labels
 
 if __name__ == "__main__":
     # preprocess()
